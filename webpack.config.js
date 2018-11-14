5b3adfba1ca95a1e72586096251e765b5ce40ba9
cdrainxv <cdrainxv@users.noreply.github.com>
2018-11-14 00:01:38 -0700

    init commit

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist/assets')
+  },
+  devServer: {
+    contentBase: [path.join(__dirname, 'dist')]
+    hot: true
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()]
+}
